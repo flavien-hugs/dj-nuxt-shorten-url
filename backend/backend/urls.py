97bc30c2348c52156268e5df32a2587924f10662
flavien-hugs <geekactux@gmail.com>
2022-06-27 18:07:50 +0000

    edit global urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,21 +1,22 @@
-"""backend URL Configuration
+# backend.urls.py
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
+
+from shortenurl.views import redirector_view
+
+redirect_to_api = RedirectView.as_view(url='api/')
+
 
 urlpatterns = [
+    path(route='', view=redirect_to_api),
+    path('api/', include('shortenurl.urls')),
+    path(
+        route='<str:shortener_link>/',
+        view=redirector_view,
+        name='redirector'
+    ),
+
     path('admin/', admin.site.urls),
 ]
